a14a027ff5e87ec66ea09e29327737b1ba4c478c
liulu1998 <1274272851@qq.com>
2020-05-08 09:38:52 +0800

    增加 无头静默运行 功能

--- main.py
+++ main.py
@@ -6,11 +6,12 @@
 import time
 import json
 from json import JSONDecodeError
-from selenium.webdriver import Chrome, ChromeOptions, Firefox
+from selenium.webdriver import Chrome, ChromeOptions, Firefox, FirefoxOptions
 
 
 class User:
     def __init__(self, **kwargs):
+        self.name = kwargs["name"]
         self.username = kwargs["username"]
         self.password = kwargs["password"]
 
@@ -25,13 +26,12 @@ class Spider:
             return
 
         # browser type
-        type = info["browserType"]
-        if type not in ["chrome", "firefox"]:
+        type_ = info["browserType"].lower()
+        if type_ not in ["chrome", "firefox"]:
             print("不支持的浏览器类型")
             return
         # <<< if
-        self.user = User(username=info["username"], password=info["password"])
-        self.name = info["name"]
+        self.user = User(name=info["name"], username=info["username"], password=info["password"])
         # course name
         self.course = info["course"]
         # total count
@@ -40,10 +40,14 @@ class Spider:
         self.school = info["school"]
 
         # webdriver
-        if type == "chrome":
+        if type_ == "chrome":
             option = ChromeOptions()
             option.add_experimental_option('excludeSwitches', ['enable-automation'])
             option.add_experimental_option('useAutomationExtension', False)
+            # headless
+            if not info["gui"]:
+                option.add_argument('--headless')
+            # <<< if
             self.driver = Chrome(options=option)
             self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
                 "source": """
@@ -55,8 +59,11 @@ class Spider:
             self.driver.execute_cdp_cmd("Network.enable", {})
             self.driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser1"}})
         else:
-            self.driver = Firefox()
-
+            option = FirefoxOptions()
+            # headless
+            if not info["gui"]:
+                option.headless = True
+            self.driver = Firefox(options=option)
 
     def login(self) -> None:
         """ login in given account
@@ -78,7 +85,7 @@ class Spider:
 
     def handle_one(self, ele) -> bool:
         """ answer one question
-        :param ele_: tag in DOM
+        :param ele: tag in DOM
         :return whether it is successful
         """
         # get question id then build new URL
@@ -93,8 +100,9 @@ class Spider:
         self.driver.switch_to.window(self.driver.window_handles[1])
 
         try:
-            # get first answer of others'
-            answer = self.driver.find_elements_by_css_selector("#answer_lab > *")[0].find_element_by_css_selector("div > div > pre").text
+            # get first answer of others
+            answer = self.driver.find_elements_by_css_selector("#answer_lab > *")[0].\
+                find_element_by_css_selector("div > div > pre").text
             # input answer and submit
             self.driver.find_element_by_id("show_answer_1").click()
         except:
@@ -107,10 +115,9 @@ class Spider:
         time.sleep(1)
 
         # 为自己点赞
-        # 直接点击
         # self.driver.find_element_by_css_selector("span.option-zan").click()
 
-        # JS 点击
+        # using JavaScript
         js = 'document.getElementsByClassName("option-zan")[1].click()'
         self.driver.execute_script(js)
         time.sleep(0.5)
@@ -160,7 +167,7 @@ class Spider:
                 break
         # <<< for
         self.driver.quit()
-        log = f"你的名字: {self.name}  课程: {self.course}  成功复读题目数: {cnt}\n"
+        log = f"你的名字: {self.user.name}  课程: {self.course}  成功复读题目数: {cnt}\n"
         print(log)
 
 
