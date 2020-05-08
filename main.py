e45aefb1476f97c9006cfc3a8fc0c30fde875687
liuchongpeng <522087905yy>
2020-05-08 12:05:47 +0800

    add function: Support multiple course comment in one round

--- main.py
+++ main.py
@@ -138,38 +138,41 @@ class Spider:
 
         # select certain course
         courses = self.driver.find_elements_by_css_selector("li.clearfix > div")
-        for c in courses:
-            if self.course in c.get_attribute("title"):
-                self.driver.execute_script("arguments[0].click();", c)
-                break
-        # <<< for
 
-        time.sleep(0.5)
-        # <<< select given course
+        for now_proceed_course in self.course:
+            for c in courses:
+                if now_proceed_course in c.get_attribute("title"):
+                    self.driver.execute_script("arguments[0].click();", c)
+                    break
+            # <<< for
 
-        # roll down to load more questions
-        for i in range(int(8 * self.count / 10)):
-            self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-            time.sleep(1)
-        # <<< for
-        questions = self.driver.find_elements_by_css_selector("#lateList >*")
-        cnt = 0
+            time.sleep(0.5)
+            # <<< select given course
 
-        for q in questions:
-            # count of existing answers
-            n_answers = q.find_element_by_css_selector(".qa_topic_reaction").\
-                find_element_by_css_selector(".qa_topic_answerNum").text
-            n_answers = int(n_answers)
+            # roll down to load more questions
+            for i in range(int(8 * self.count / 10)):
+                self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+                time.sleep(1)
+            # <<< for
+            questions = self.driver.find_elements_by_css_selector("#lateList >*")
+            cnt = 0
 
-            if 0 < n_answers <= 100:
-                if self.handle_one(q):
-                    cnt += 1
-            if cnt >= self.count:
-                break
-        # <<< for
+            for q in questions:
+                # count of existing answers
+                n_answers = q.find_element_by_css_selector(".qa_topic_reaction").\
+                    find_element_by_css_selector(".qa_topic_answerNum").text
+                n_answers = int(n_answers)
+
+                if 0 < n_answers <= 100:
+                    if self.handle_one(q):
+                        cnt += 1
+                if cnt >= self.count:
+                    break
+            # <<< for
+            log = f"你的名字: {self.user.name}  课程: {self.course}  成功复读题目数: {cnt}\n"
+            print(log)
+
         self.driver.quit()
-        log = f"你的名字: {self.user.name}  课程: {self.course}  成功复读题目数: {cnt}\n"
-        print(log)
 
 
 if __name__ == '__main__':
