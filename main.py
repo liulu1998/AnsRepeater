3ce68c35947354dfb202f4cbe2bb9b08982e857c
liulu1998 <1274272851@qq.com>
2020-05-10 17:59:06 +0800

    replace sleep with implicitly wait, print with logging

--- main.py
+++ main.py
@@ -4,7 +4,19 @@
 
 import time
 import json
+from json import JSONDecodeError
+import logging
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver import Chrome, ChromeOptions, Firefox, FirefoxOptions
+# exceptions selenium may raise
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+
+# 隐式等待时间
+WAIT_TIME = 3
+
+logging.basicConfig(format='%(message)s',
+                    level=logging.INFO)
 
 
 class User:
@@ -20,7 +32,7 @@ class Spider:
         # browser type
         type_ = info["browserType"].lower()
         if type_ not in ["chrome", "firefox"]:
-            print("不支持的浏览器类型")
+            logging.critical("不支持的浏览器类型")
             return
         # <<< if
         self.user = User(name=info["name"], username=info["username"], password=info["password"])
@@ -43,6 +55,9 @@ class Spider:
             # <<< if
             option.add_experimental_option("excludeSwitches", ["enable-logging"])
             self.driver = Chrome(options=option)
+            # webdriver wait for resources implicitly
+            self.driver.implicitly_wait(WAIT_TIME)
+            # make "window.navigator.webdriver = undefined" to avoid detection
             self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
                 "source": """
                         Object.defineProperty(navigator, 'webdriver', {
@@ -57,7 +72,9 @@ class Spider:
             # headless
             if not info["gui"]:
                 option.headless = True
+            # TODO close log
             self.driver = Firefox(options=option)
+            self.driver.implicitly_wait(WAIT_TIME)
 
     def login(self) -> None:
         """ login in given account
@@ -65,16 +82,13 @@ class Spider:
         """
         self.driver.get("https://passport.zhihuishu.com/login?service=https://onlineservice.zhihuishu.com/login/gologin")
         self.driver.find_element_by_id("qStudentID").click()
-        time.sleep(0.5)
         # school name
         self.driver.find_element_by_id("quickSearch").send_keys(self.school[0: len(self.school) - 1])
         schools = self.driver.find_elements_by_css_selector("#schoolListCode > *")
         schools[0].click()
         # student id and password
         self.driver.find_element_by_id("clCode").send_keys(self.user.username)
-        time.sleep(0.2)
         self.driver.find_element_by_id("clPassword").send_keys(self.user.password)
-        time.sleep(0.2)
         self.driver.find_element_by_css_selector("span.wall-sub-btn").click()
 
     def handle_one(self, ele) -> bool:
@@ -85,11 +99,10 @@ class Spider:
         # get question id then build new URL
         id_ = int(ele.find_element_by_css_selector("a > p").get_attribute("data-question-id"))
         url = f"https://wenda.zhihuishu.com/shareCourse/questionDetailPage?sourceType=2&qid={id_}"
-        time.sleep(0.5)
+
         # open new tab
         js = f"window.open('{url}')"
         self.driver.execute_script(js)
-        time.sleep(2)
         # switch to new tab
         self.driver.switch_to.window(self.driver.window_handles[1])
 
@@ -97,22 +110,22 @@ class Spider:
             # get first answer of others
             answer = self.driver.find_elements_by_css_selector("#answer_lab > *")[-1].\
                 find_element_by_css_selector("div > div > pre").text
-            # input answer and submit
             self.driver.find_element_by_id("show_answer_1").click()
-        except:
+        # catch two kinds of exception
+        except (ElementNotInteractableException, NoSuchElementException, IndexError):
             self.driver.close()
             self.driver.switch_to.window(self.driver.window_handles[0])
             return False
         # <<< try-except
+
+        # input answer and submit
         self.driver.find_element_by_css_selector("textarea.my-ans-textarea").send_keys(answer)
         self.driver.find_element_by_id("answer_save_zz").click()
-        time.sleep(1)
 
-        # 为自己点赞
+        # give yourself a Praise
         # using JavaScript
         js = 'document.getElementsByClassName("option-zan")[1].click()'
         self.driver.execute_script(js)
-        time.sleep(0.5)
 
         # close new tab and back to main tab
         self.driver.close()
@@ -137,14 +150,9 @@ class Spider:
                     self.driver.execute_script("arguments[0].click();", c)
                     break
             # <<< for c in courses
-
-            time.sleep(0.5)
-            # <<< select given course
-
             # roll down to load more questions
             for i in range(int(8 * self.count / 10)):
                 self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-                time.sleep(1)
             # <<< for
             questions = self.driver.find_elements_by_css_selector("#lateList >*")
             cnt = 0
@@ -166,16 +174,25 @@ class Spider:
                     break
             # <<< for q in questions
             log = f"你的名字: {self.user.name}  课程: {now_proceed_course}  成功复读题目数: {cnt}\n"
-            print(log)
+            logging.info(log)
         # <<< for, handle multiple courses
         self.driver.quit()
 
 
 if __name__ == '__main__':
+
     try:
         with open("./info.json", "r", encoding="utf-8") as f:
             info = json.load(f)
-        spider = Spider(info)
+    except FileNotFoundError:
+        logging.critical("info.json 未找到")
+    except JSONDecodeError:
+        logging.critical("info.json 解码出错")
+
+    logging.info("任务开始")
+
+    spider = Spider(info)
+    try:
         spider.solve()
-    except:
-        print("解析出错")
+    except NoSuchElementException:
+        logging.error("失败! 请检查网络状况")
